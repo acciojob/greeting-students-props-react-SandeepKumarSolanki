0bf85b1eef591aeac88eb2d25fbd8a278821572a
SandeepKumar <sandeepkusolanki@gamil.com>
2025-04-05 01:24:31 +0530

    just change the name

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Welcome = ({name}) => {
   return (
     <div>
-      <h1>Hey ! {name}</h1>
+      <h1>Hey !</h1>
       <h2>Welcome to School.</h2>
       <p>It is a wonderful experience</p>
     </div>
